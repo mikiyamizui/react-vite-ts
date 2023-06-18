fa8f9d72a7d83171bb47cb2ef4a2c7aab1446db6
Mikiya Mizui <mikiya.mizui@gmail.com>
2023-06-19 00:06:00 +0900

    prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,19 +1,20 @@
-module.exports =
-  /** @type {import("prettier").Config} */
-  ({
-    printWidth: 120,
-    tabWidth: 2,
-    useTabs: false,
-    semi: true,
-    singleQuote: false,
-    quoteProps: "as-needed",
-    jsxSingleQuote: false,
-    trailingComma: "es5",
-    bracketSpacing: true,
-    bracketSameLine: false,
-    arrowParens: "always",
-    proseWrap: "preserve",
-    htmlWhitespaceSensitivity: "css",
-    endOfLine: "lf",
-    singleAttributePerLine: true,
-  });
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  singleAttributePerLine: true,
+};
+
+module.exports = config;
